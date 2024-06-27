8445a2dc4c299b2409398027f4d1d52b5dece470
Otalora, Nicolas <nicolas.otalora@globant.com>
2024-06-27 17:53:20 -0500

    Added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,14 @@ function run() {
   const distFolder = core.getInput('dist-folder', { required: false });
 
   // 2) Upload files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
 
   core.notice('Hello from my custom JS action');
+
+  const websiteUrl = `https://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl);
 }
 
 run();
